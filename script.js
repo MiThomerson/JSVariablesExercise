b5465c9aafe384e81256002a685740a802e7552a
Mic Thomerson <mic732@gmail.com>
2023-03-19 20:14:38 -0500

    Finished Exercise.

---
+++ script.js
@@ -0,0 +1,53 @@
+// 1
+// const
+
+// 2
+let num = 20;
+console.log(num);
+
+// 3
+num = 15;
+console.log(num);
+
+//4
+num += 7;
+console.log(num);
+
+// 5
+num--;
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+console.log(blank);
+
+// BONUS
+// 1
+// const quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// OR
+const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+// 2
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+// let pSpot = "qwertyuiop".length;
+// pSpot--;
+// console.log(letters [pSpot]);
+
+// OR
+
+console.log(letters[letters.length - 1]);
